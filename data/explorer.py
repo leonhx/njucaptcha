85bd738a1638952a28132e5cf967adb8a4a5a5ab
leon-huang <leon@njuopen.com>
2014-03-16 15:00:55 +0800

    change explorer

--- data/explorer.py
+++ data/explorer.py
@@ -4,16 +4,14 @@
 import numpy as np
 import pylab as pl
 
-captchas = np.load('raw_captchas.npy')
-# captchas[captchas < 127] = 0
-# captchas[captchas >= 127] = 255
-captchas.shape = len(captchas), 100, 210
+import time
+
+captchas = np.load('clean_chars.npy')
+captchas.shape = -1, 40, 40
 
 for c in captchas:
     pl.figure(1)
     pl.imshow(c, cmap=pl.cm.Greys)
     pl.show()
-    if raw_input() == 'q':
-        pl.close(1)
-        break
+    time.sleep(1)
     pl.close(1)
